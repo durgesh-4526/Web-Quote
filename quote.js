d15d960a8e52bfa0dd0491a9452221fd321750f9
Durgesh Sahu <durgeshsahu8006@gmail.com>
2021-11-23 17:10:28 +0530

    Quote

---
+++ quote.js
@@ -0,0 +1,16 @@
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
+
+const url = "https://api.quotable.io/random";
+
+let getquote = () => {
+    fetch(url)
+    .then((data) => data.json())
+    .then((item) =>{
+        quote.innerHTML = item.content;
+        author.innerHTML = item.author;
+    })
+}
+window.addEventListener("load",getquote);
+btn.addEventListener("click",getquote);
